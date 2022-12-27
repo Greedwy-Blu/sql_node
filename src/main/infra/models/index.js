6dc7f74911762709043fef3b0fe3ea615b35b4e2
Greedwy-Blu <greedwyblu@gmail.com>
2022-12-27 15:55:05 -0300

    update

--- src/main/infra/models/index.js
+++ src/main/infra/models/index.js
@@ -1,6 +1,11 @@
 'use strict';
 const dbConfig = require("../../config/database");
 
+const fs = require('fs');
+const path = require('path');
+const basename = path.basename(__filename);
+const bcrypt = require('bcrypt')
+
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
   host: dbConfig.host,
@@ -10,6 +15,24 @@ const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.p
 });
 
 const db = {};
+
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
