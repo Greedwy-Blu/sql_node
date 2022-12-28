03569588b777b2bfe8e372b4ffb0743940162c96
Greedwy-Blu <greedwyblu@gmail.com>
2022-12-28 20:27:36 -0300

    check

--- src/server.js
+++ src/server.js
@@ -1,51 +1,50 @@
-const express = require('express');
-const cors = require('cors');
-const { ApolloServer, gql } = require('apollo-server-express');
-const http = require("http");
-const routes = require('./main/router/routes');
+const express = require('express')
+const cors = require('cors')
+const { ApolloServer } = require('apollo-server-express')
+const http = require('http')
+const routes = require('./main/router/routes')
 const db = require('./main/infra/models/index')
 
-db.sequelize.authenticate().then(() =>{//Conexion a la BD
-    console.log("Estas conectado a la BD")
-});
+db.sequelize.authenticate().then(() => {
+  console.log('Estas conectado a la BD')
+})
 db.sequelize.sync()
 
-
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 
 const typeDefs = `
     type Query{
         totalPosts: Int!
     }
-`;
+`
 const resolvers = {
-    Query: {
-        totalPosts: () => 100,
-    },
-};
-
-let apolloServer = null;
-
-async function startServer() {
-    apolloServer = new ApolloServer({
-        typeDefs,
-        resolvers,
-         context: { db },
-    });
-    await apolloServer.start();
-    apolloServer.applyMiddleware({ app });
+  Query: {
+    totalPosts: () => 100
+  }
 }
 
-startServer();
-const httpserver = http.createServer(app);
+let apolloServer = null
 
-app.use(routes);
-app.get("/rest", function (req, res) {
-    res.json({ data: "api working" });
-});
+async function startServer () {
+  apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: { db }
+  })
+  await apolloServer.start()
+  apolloServer.applyMiddleware({ app })
+}
+
+startServer()
+const httpserver = http.createServer(app)
+
+app.use(routes)
+app.get('/rest', function (req, res) {
+  res.json({ data: 'api working' })
+})
 
 app.listen(4000, function () {
-    console.log(`server running on port 4000`);
-    console.log(`gql path is ${apolloServer.graphqlPath}`);
-});
+  console.log('server running on port 4000')
+  console.log(`gql path is ${apolloServer.graphqlPath}`)
+})
